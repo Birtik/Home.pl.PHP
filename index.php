bc1061529e81467be34067fafa3e7ba7326e96d4
bartek8654 <40451206+bartek8654@users.noreply.github.com>
2019-06-05 00:19:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,50 +12,95 @@
 		public function createCircle()
 		{
 			$range = $this->r;
-			for($j=0;$j<=($range);$j++)
-			{
+			$plus = $range - 1;
+			$fall = 0;
+			
+			if($plus%2==0) $plus++;
+			if($range%2==1) $fall = $range - 1;
+			else $fall = $range;
 				
-				for($i=0;$i<$range-$j;$i++)
+			
+			for($i=0;$i<$range;$i++)
+			{
+				echo " ";
+			}
+			for($i=0;$i<$plus;$i++)
+			{
+				echo "+";
+			}
+			echo "\r\n";
+			
+			$begin = 0;
+			$end = 0;
+			
+			for($i=0;$i<($fall - ($fall/2));$i++)
+			{
+				for($j=0;$j<$range-$i-1;$j++)
 				{
 					echo " ";
 				}
 				echo "+";
 				
-				if($j!=0)
-				{
-					for($i=0;$i<(($j*2) - 1);$i++)
-					{
-						echo ".";
-					}
-					echo "+";
-				}
-				echo "\r\n";
-			}
-		
-		
-			for($j=1;$j<=($range);$j++)
-			{
-				
-				for($i=0;$i<$j;$i++)
+				for($j=0;$j<(($i*2)+$plus);$j++)
 				{
 					echo " ";
 				}
 				echo "+";
 				
-				if($j!=$range)
-				{
-					for($i=0;$i<((2*$range)-$j*2)-1;$i++)
-					{
-						echo ".";
-					}
-					echo "+";
+				echo "\r\n";
 				
+				$begin = $range-$i-1;
+				$end = (($i*2)+$plus);
+			}
+			
+			
+			for($j=0;$j<$plus;$j++)
+			{
+				for($i=0;$i<$begin-1;$i++)
+				{
+					echo " ";
 				}
+				echo "+";
+				
+				for($i=0;$i<$end+2;$i++)
+				{
+					echo " ";
+				}
+				echo "+";
 				echo "\r\n";
 			}
-
+			
+			$begin2 = 0;
+			
+			for($j=0;$j<($fall - ($fall/2));$j++)
+			{
+				for($i=0;$i<$begin+$j;$i++)
+				{
+					echo " ";
+				}
+				echo "+";
+				
+				for($i=0;$i<$end-$j*2;$i++)
+				{
+					echo " ";
+				}
+				echo "+";
+				
+				
+				echo "\r\n";
+				$begin2 = $begin+$j;
+			}	
+			
+			for($i=0;$i<$begin2+1;$i++)
+			{
+				echo " ";
+			}
+			for($i=0;$i<$plus;$i++)
+			{
+				echo "+";
+			}
 		}
-		
+			
 		public function __destruct(){}
 	}
 
